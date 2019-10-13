a65151cec69d9b83dcbecbf778aceda481260395
NeilDG <fm.atropos@gmail.com>
2019-10-13 12:30:27 +0800

    Added template for P2..

--- src/algocom_pe_test/algocom_p2/P2_Program.java
+++ src/algocom_pe_test/algocom_p2/P2_Program.java
@@ -1,141 +1,102 @@
 package algocom_pe_test.algocom_p2;
-/*
- * Ronn Christian C. Chua Chiaco
- * PE2
- * CSALGCM S16
- *
- */
 import java.util.*;
 
-public class P2_Program {
-
-
-	//#1
+public class P2_Program
+{
     public static void analyze (String input)
     {
-    	class listelem {
-    		public char ch;
-    		public int count;
+        ArrayList<Integer> value = new ArrayList<>();
+        int index = 0;
+        int temp = 0;
+        char[] letters = (input.toUpperCase()).toCharArray();
 
-    		listelem (char c, int n)
-    		{
-    			ch = c;
-    			count = n;
-    		}
-    	}
+        for (int i = 0; i < 26; i++)
+            value.add(0);
+        
+        for (int i = 0; i < letters.length; i++){
+            if (Character.isLetter(letters[i])){
+                index = letters[i] - 65;
+                temp = value.get(index);
+                temp ++;
+                value.set(index, temp);
+            }
+        }
 
-    	int charctr, printctr, ctr, index;
-    	ArrayList<listelem> list = new ArrayList<>();
-    	input = input.trim();
-    	input = input.toUpperCase();
+        int mode = 0;
+        for (int i= 1; i < value.size(); i++)
+            if (value.get(mode) < value.get(i))
+                mode = i;
+        
+        int count = value.get(mode);
 
-		for (ctr = 0; ctr < 26; ctr++)
-			list.add(new listelem('?', 0));
-
-    	for (charctr = 0; charctr < input.length(); charctr++)
-    	{
-			if (Character.isLetter(input.charAt(charctr)) == true)
-			{
-				index = (int)input.charAt(charctr) - 65;
-				if (list.get(index).count == 0)
-					list.get(index).ch = input.charAt(charctr);
-
-				list.get(index).count++;
-			}
-    	}
-
-		Collections.sort(list, new Comparator<listelem>()
-		{
-			@Override
-			public int compare(listelem elem1, listelem elem2)
-			{
-				char ch1 = elem1.ch, ch2 = elem2.ch;
-				int num1 = elem1.count, num2 = elem2.count;
-				int comp;
-
-				comp = Integer.valueOf(num1).compareTo(Integer.valueOf(num2));
-				if (comp == 0)
-				{
-					comp = Character.valueOf(ch1).compareTo(Character.valueOf(ch2));
-					if (comp > 0)
-						comp = -1;
-					else if (comp < 0)
-						comp = 1;
-				}
-
-				return comp;
-			}
-		});
-
-		for (printctr = list.size()-1; printctr > 0; printctr--)
-			if (list.get(printctr).count != 0)
-				System.out.println(list.get(printctr).ch + " " + list.get(printctr).count);
+        while (count != 0){
+            for (int i = 0; i < value.size(); i++)
+                if (value.get(i) == count)
+                    System.out.println ((char)(i+65) + " " + value.get(i));
+            count -- ;
+        }
     }
 
-    //#2
-    public static void countSwaps (int length, int[] carriageNumbers)
+    public static void countSwaps (int length, int[] carraigeNumbers)
     {
-		int i, j, temp, sorts = 0;
+        int count = 0;
+        int temp;
+        for (int i = 0; i < length-1; i++){
+            for (int j = 0; j < length - i - 1; j++){
+                if (carraigeNumbers[j] > carraigeNumbers[j+1]){
+                    temp = carraigeNumbers[j];
+                    carraigeNumbers[j] = carraigeNumbers[j+1];
+                    carraigeNumbers[j+1] = temp;
+                    count++;
+                }
+            }
+        }
 
-		for (i = 0; i < length-1; i++)
-			for (j = 0; j < length-i-1; j++)
-				if (carriageNumbers[j] > carriageNumbers[j+1])
-				{
-					temp = carriageNumbers[j];
-					carriageNumbers[j] = carriageNumbers[j+1];
-					carriageNumbers[j+1] = temp;
-					sorts++;
-				}
-
-		System.out.println ("The most optimal train swaps: " + sorts);
+        System.out.println ("The most optimal train swaps: " + count);
     }
 
-    //#3
     public static void checkHarvest (int numVillages, int costToFeed, int[] harvests)
     {
-    	int totalFeed = numVillages * costToFeed, totalHarvest = 0, ctr;
-    	String text = new String();
+        int demand = numVillages * costToFeed;
+        int supply = 0;
 
-    	for (ctr = 0; ctr < numVillages; ctr++)
-    		totalHarvest += harvests[ctr];
-
-    	if (totalFeed > totalHarvest)
-    		text = "NOT ENOUGH FOOD";
-    	else if (totalFeed == totalHarvest)
-    		text = "JUST ENOUGH FOR EVERYONE";
-    	else
-    		text = "PARTY!";
-
-    	System.out.println (text);
+        for (int i = 0; i < numVillages; i++)
+            supply += harvests[i];
+        
+        if (demand == supply)
+            System.out.println ("JUST ENOUGH FOR EVERYONE");
+        else if (demand < supply)
+            System.out.println ("PARTY!");
+        else if (demand > supply)
+            System.out.println ("NOT ENOUGH FOOD");
     }
 
-    //#4
-    public static void printCombination (int N, int K)
-    {
-    	int ctr;
-    	int combination[] = new int[K], nums[] = new int[N];
+    public static void printCombination(int N, int K) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
 
-    	for (ctr = 1; ctr <= N; ctr++)
-    		nums[ctr - 1] = ctr;
+        ArrayList<Integer> perLine = new ArrayList<>();
 
-		makeCombination (nums, combination, 0, N-1, 0, K);
+        combination(N,K,1, perLine, result);
+
+        for (int i = 0; i < result.size(); i++){
+            ArrayList<Integer> list = result.get(i);
+            for (int j = 0; j < list.size(); j++){
+                System.out.print (list.get(j) + " ");
+            }
+            System.out.println ();
+        }
     }
 
-    public static void makeCombination (int nums[], int combination[], int start, int end, int ind, int K)
-    {
-        if (ind == K)
-        {
-            for (int j=0; j<K; j++)
-                System.out.print(combination[j] + " ");
-
-            System.out.println("");
+    private static void combination (int N, int K, int start, ArrayList<Integer> perLine, ArrayList<ArrayList<Integer>> result) {
+        if(perLine.size() == K) {
+            result.add(new ArrayList<>(perLine));
             return;
         }
 
-        for (int i=start; i<=end && end-i+1 >= K-ind; i++)
-        {
-            combination[ind] = nums[i];
-            makeCombination(nums, combination, i+1, end, ind+1, K);
+        for(int i = start; i <= N; i++) {
+            perLine.add(i);
+            combination(N, K, i+1, perLine, result);
+            perLine.remove(perLine.size() - 1);
         }
     }
 }
