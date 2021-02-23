281b4cb285bb717a8a61f92ff7e203ea2089d95d
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2021-02-23 21:03:07 +0800

    Updated code for P5.

--- src/algocom_pe_test/p5_problems/P5Code.java
+++ src/algocom_pe_test/p5_problems/P5Code.java
@@ -1,247 +1,231 @@
 package algocom_pe_test.p5_problems;
 
 public class P5Code {
-	
-	
-	public static void main (String [] args){
-		///////////number 1///////////
-//		String [] arr = {"A 10 30",
-//						 "B 30 5",
-//						 "C 5 60"};
-		
-//		String [] arr = {"A 4 10",
-//						 "B 10 3",
-//						 "C 3 12",
-//						 "D 12 20",
-//						 "E 20 7"};
-		
-//		String [] arr = {"A 40 20",
-//		 "B 20 30",
-//		 "C 30 10",
-//		 "D 10 30"};
-		
-/////////IF KASAMA FIRST LINE:
-		String [] arr = {"3",
-						 "A 10 30",
-		 				 "B 30 5",
-		 				 "C 5 60"};
-
-//		String [] arr = {"5", 
-//						 "A 4 10",
-//						 "B 10 3",
-//						 "C 3 12",
-//						 "D 12 20",
-//						 "E 20 7"};
-		
-//		String [] arr = {"4",
-//						 "A 40 20",
-//						 "B 20 30",
-//						 "C 30 10",
-//						 "D 10 30"};
-		assignTable(arr);
-
-		///////////number 2///////////
-		String [] partyInputs = {"50 10",
-								 "12 3",
-								 "15 8",
-								 "16 9",
-								 "16 6",
-								 "10 2",
-								 "21 9",
-								 "18 4",
-								 "12 4",
-								 "17 8",
-								 "18 9"};
-
-//		String[] partyInputs = {"50 10",
-//								"13 8",
-//								"19 10",
-//								"16 8",
-//								"12 9",
-//								"10 2",
-//								"12 8",
-//								"13 5",
-//								"15 5",
-//								"11 7",
-//								"16 2"};
-		
-        partyBudget(partyInputs);
-		
-		///////////number 3///////////
-//        int [] places = {25, 50, 75};
-//        cut(100, 3, places);     
-
-        int [] places = {4, 5, 7, 8};
-        cut(10, 4, places);
-        
-	}
-	
-	////////////////////////////////number 1 code ////////////////////////////////
 	public static void assignTable(String[] input) {
-		//there will be N inputs. Each input is space-separated into A,B and C.
-		//Read the input, parse and represent in your own preference.
-		//your print statements here
-		
-//		int newArr [] = new int [input.length+1]; 
-		int newArr [] = new int [input.length]; //IF KASAMA FIRST LINE:
-		int ind = 0;
-//		for (int i=0; i<input.length; i++){
-		for (int i=1; i<input.length; i++){ //IF KASAMA FIRST LINE:
-			String [] tempStr = input[i].split(" ");
-//			System.out.println(tempStr[0]+"_"+tempStr[1]+"_"+tempStr[2]);
-			
-//			if (i==0) {
-			if (i==1) { //IF KASAMA FIRST LINE:
-				newArr[ind] = Integer.parseInt(tempStr[1]);
-				ind++;
-				newArr[ind] = Integer.parseInt(tempStr[2]);
-				ind++;
-//				System.out.print(Integer.parseInt(tempStr[1])+" "+Integer.parseInt(tempStr[2])+" ");
-			}
-			else {
-				newArr[ind] = Integer.parseInt(tempStr[2]);
-				ind++;
-//				System.out.print(Integer.parseInt(tempStr[2])+" ");
-			}
- 		}
-//		System.out.println();
-		
-		
-		int n = newArr.length-1;
-		int [][] mat = new int[n][n];
-		int [][] store = new int[n][n];
-		
-		for (int i=0; i<n; i++) { 
-			mat[i][i] = 0;
-		}
-		
-		for (int row=1; row<n; row++) {
-			for (int i=0; i<n-row; i++) { 
-				int j = i + row;
-				mat[i][j] = Integer.MAX_VALUE; //set to a large value
-				for (int k=i; k<j; k++) {
-					//calculate cost
-					int cost = mat[i][k] + mat[k+1][j] + newArr[i] * newArr[k+1] * newArr[j+1];
-					
-					if (k == i) {
-						mat[i][j] = cost;
-						store[i][j] = k;
-					} else if (k == i + 1) {
-						if (mat[i][j]>cost) {
-							mat[i][j] = cost;
-							store[i][j] = k;
-						}
-					} else {
-						if (cost<mat[i][j]) {
-							mat[i][j] = cost;
-							store[i][j] = k;
-						}
-					}
-				}
-			}
-		}
-		
-		int parenCnt = 0;
-		printOptParenthesis(store, 0, n-1, parenCnt);
-		System.out.println();
-	}
-	
-	static void printOptParenthesis(int [][]s, int i, int j, int cnt) { 
-		if (i == j) {
-			int a= 65+i;
-			char c=(char)(a);			
-			System.out.print(c+""); 
-			cnt++;
-		} else {			
-			if (cnt==0){
-				cnt++;
-				printOptParenthesis(s, i, s[i][j], cnt);
-				printOptParenthesis(s, s[i][j] + 1, j, cnt);			
-			}
-			else {
-				cnt++;
-				System.out.print("(");
-				printOptParenthesis(s, i, s[i][j], cnt);
-				printOptParenthesis(s, s[i][j] + 1, j, cnt);
-				System.out.print(")");				
-			}			
-		}		
-	} 
+        int numOfMatrix = Integer.parseInt(input[0]);
+        int table [] = new int [numOfMatrix+1];
+        int ctr = 0;
+        for (int i=0; i<numOfMatrix; i++){
+            String [] inputCut = input[i+1].split(" ");
+//			System.out.println(inputCut[0]+"_"+inputCut[1]+"_"+inputCut[2]);
 
-	////////////////////////////////number 2 code ////////////////////////////////
-	public static void partyBudget(String[] inputs) {
-		//there will be N inputs. Each input is space-separated into entrance fee, and fun rating.
-		//Read the input, parse and represent in your own preference.
-		//your print statements here 
-		
-		String [] tempArr1 = inputs[0].split(" ");
-        int maximumCost = Integer.parseInt(tempArr1[0]);
-        int numberOfParties = Integer.parseInt(tempArr1[1]);
-
-        int[] funArray = new int[numberOfParties];
-        int[] feeArray = new int[numberOfParties]; 
-
-        for(int i=0; i<numberOfParties; i++) {
-        	String [] tempArr2 = inputs[i+1].split(" ");
-        	funArray[i] = Integer.parseInt(tempArr2[1]);
-        	feeArray[i] = Integer.parseInt(tempArr2[0]);
+            if (i==0){
+                table[ctr] = Integer.parseInt(inputCut[1]);
+                ctr++;
+                table[ctr] = Integer.parseInt(inputCut[2]);
+                ctr++;
+//                System.out.print(Integer.parseInt(inputCut[1])+" "+Integer.parseInt(inputCut[2])+" ");
+            }
+            else {
+                table[ctr] = Integer.parseInt(inputCut[2]);
+                ctr++;
+//                System.out.print(Integer.parseInt(inputCut[2])+" ");
+            }
         }
-        
-        int[][] M = new int[funArray.length+1][maximumCost+1];
-        
-        for (int i=1; i<=funArray.length; i++) {
-        	for (int j=0; j<=maximumCost; j++) {
-                if (feeArray[i-1]>j) {
-                    M[i][j] = M[i-1][j];
-                } else {
-                	M[i][j] = Integer.max(M[i-1][j],
-                    M[i-1][j-feeArray[i-1]] + funArray[i-1]);
+
+//        System.out.println();
+
+
+        int n = table.length-1;
+        int [][] m = new int[n][n];
+        int [][] s = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            m[i][i] = 0;
+        }
+
+        for (int row = 1; row < n; row++) {
+            for (int i = 0; i < n - row; i++) {
+                int j = i + row;
+                m[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int q = m[i][k] + m[k + 1][j] + table[i]* table[k + 1] * table[j + 1];
+                    if (k == i) {
+                        m[i][j] = q;
+                        s[i][j] = k;
+                    } 	else if (k == i + 1) {
+                        if (m[i][j] > q) {
+                            m[i][j] = q;
+                            s[i][j] = k;
+                        }
+                    } else {
+                        if (q < m[i][j]) {
+                            m[i][j] = q;
+                            s[i][j] = k;
+                        }
+                    }
+                }
+
+                if (row == table.length - 2) {
+//                    System.out.println("\nOptimal Cost is : " + m[i][j]);
                 }
             }
         }
 
-        int n = funArray.length;
-        int res = M[n][maximumCost];
-        int totalFees = 0;
-        int mc = maximumCost;
-        
-        for (int i=n; i>0 && res>0; i--) {
-        	if (!(res==M[i-1][mc])){
-            	totalFees += feeArray[i-1];
-                res = res-funArray[i-1];
-                mc = mc - feeArray[i-1];
-            }
-        	else {
-        		continue;
-        	}
-        }
-        System.out.println(totalFees + " " + M[funArray.length][maximumCost]);
-	}
+        int startPrint = 0;
+        printParenthesis(s, 0, n-1, startPrint);
+        System.out.println();
+//        System.out.println();
+    }
 
-	////////////////////////////////number 3 code ////////////////////////////////
-	public static void cut (int l, int cuts, int[] places) {		
-		int[] cutsPlaces = new int[cuts+2];
-        for (int i=1; i<=cuts; i++) {
-        	cutsPlaces[i] = places[i-1];
+    static void printParenthesis(int [][]s, int i, int j, int ctr) {
+        if (i == j) {
+            int a= 65+i;
+            char c=(char)(a);
+            System.out.print(c+"");
+            ctr++;
+        } else {
+            if (ctr==0){
+                ctr++;
+                printParenthesis(s, i, s[i][j], ctr);
+                printParenthesis(s, s[i][j] + 1, j, ctr);
+            }
+            else {
+                ctr++;
+                System.out.print("(");
+                printParenthesis(s, i, s[i][j], ctr);
+                printParenthesis(s, s[i][j] + 1, j, ctr);
+                System.out.print(")");
+            }
         }
-        cutsPlaces[cuts+1] = l;
-	
-	    int matrix[][] = new int[cuts+2][cuts+2];
-	    
-	    for (int i=2; i<cutsPlaces.length; i++) {
-	        for (int j=0; j<cutsPlaces.length-i; j++) {
-	            int k = i+j;
-	            int minCut = Integer.MAX_VALUE;
-	            
-	            for (int x=j+1; x<k; x++) {
-	            	minCut = Math.min(minCut, matrix[j][x] + matrix[x][k]);
-	            }	            
-	            matrix[j][k] = minCut + cutsPlaces[k] - cutsPlaces[j];
-	        }
-	    }
-	
-	    System.out.println("The minimum cutting is "+matrix[0][cuts+1]);	
-	}
+
+    }
+
+
+    public static void partyBudget(String[] inputs) {
+
+        int maxCost = Integer.parseInt(inputs[0].split(" ")[0]);
+        int numParties = Integer.parseInt(inputs[0].split(" ")[1]);
+
+//        System.out.println(maxCost);
+//        System.out.println(numParties);
+
+        int[] funArr = new int[numParties]; // v
+        int[] costArr = new int[numParties]; // w
+
+        for(int i=0; i<numParties; i++){
+            int cost = Integer.parseInt(inputs[i+1].split(" ")[0]);
+            int fun = Integer.parseInt(inputs[i+1].split(" ")[1]);
+//            System.out.println(cost + " - " + fun);
+
+            funArr[i] = fun;
+            costArr[i] = cost;
+        }
+        knapSack(funArr, costArr, maxCost);
+    }
+
+    public static void knapSack(int[] funArr, int[] costArr, int W)
+    {
+
+        int numParties = funArr.length;
+        int[][] Table = new int[funArr.length + 1][W + 1];
+
+        for (int i = 1; i <= funArr.length; i++)
+        {
+            for (int j = 0; j <= W; j++)
+            {
+
+//                System.out.println("I = " + i + " J = " + j); // indexes
+//                System.out.println(costArr[i-1] + " > " + j); // checking cost of
+                if (costArr[i-1] > j) {
+//                    System.out.println("Table[i-1][j] = " + Table[i-1][j]);
+                    Table[i][j] = Table[i-1][j];
+                } else {
+//                    System.out.println("Table[i][j] = " + Integer.max(Table[i-1][j], Table[i-1][j-costArr[i-1]] + funArr[i-1]));
+//                    System.out.println("Get max of " + Table[i-1][j] + " or " + Table[i-1][j-costArr[i-1]] + funArr[i-1]);
+                    Table[i][j] = Integer.max(Table[i-1][j],
+                            Table[i-1][j-costArr[i-1]] + funArr[i-1]);
+                }
+//                System.out.println();
+            }
+        }
+
+        int total = Table[numParties][W];
+//        System.out.println(total);
+        int totalCost = 0;
+        int w = W;
+        for (int i = numParties; i > 0 && total > 0; i--) {
+
+//            System.out.println("i = " + i + " w = " + w + " |||| Res = " + res + " T[i-1][w] = " + T[i - 1][w]);
+
+            if (total == Table[i - 1][w]) {
+
+            }
+            else {
+//                System.out.println(costArr[i - 1] + " ");
+                totalCost += costArr[i-1];
+
+//                System.out.println(costArr[i-1]  + " " + funArr[i-1] );
+                total = total - funArr[i - 1];
+                w = w - costArr[i - 1];
+            }
+        }
+//
+//        System.out.println();
+//        System.out.println();
+
+//        for (int i = 1; i <= funArr.length; i++) {
+//            // consider all weights from 0 to maximum capacity W
+//            for (int j = 0; j <= W; j++) {
+//                System.out.print(T[i][j] + "    ");
+//            }
+//            System.out.print("  I = " + i);
+//
+//            System.out.println();
+//
+//        }
+//        System.out.println("-------------------------------------------------");
+//        for (int i=0; i<=W; i++){
+//            System.out.print(i);
+//            if(i < 10)
+//                System.out.print("    ");
+//            else if(i< 25)
+//                System.out.print("   ");
+//            else
+//                System.out.print("    ");
+//
+//        }
+
+
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+        System.out.println(totalCost + " " + Table[funArr.length][W]);
+    }
+
+
+    public static void cut (int l, int cuts, int[] places) {
+
+        int[] whereToCut = new int[cuts+2];
+            for (int i = 1; i <= cuts; i++)
+                whereToCut[i] = places[i-1];
+        whereToCut[cuts + 1] = l;
+
+
+        int m[][] = new int[cuts + 2][cuts + 2];
+
+        for (int z = 2; z < whereToCut.length; z++) {
+            for (int i = 0; i < whereToCut.length - z; i++) {
+                int j = z + i;
+
+                int cutMin = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++)
+                    cutMin = Math.min(cutMin, m[i][k] + m[k][j]);
+
+                m[i][j] = cutMin + whereToCut[j] - whereToCut[i];
+            }
+        }
+
+        System.out.println("The minimum cutting is " + m[0][cuts + 1]);
+
+//        for (int i=0; i<cuts+2; i++){
+//            for (int j=0; j< cuts+2; j++){
+//                System.out.print(m[i][j] + "    ");
+//            }
+//            System.out.println();
+//        }
+
+
+    }
 
 }
 
--- src/algocom_pe_test/p5_problems/P5_TestRunner.java
+++ src/algocom_pe_test/p5_problems/P5_TestRunner.java
@@ -5,39 +5,56 @@ public class P5_TestRunner {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		P1Test();
-		P2Test();
+		//P1Test();
+		//P2Test();
 		P3Test();
 	}
 	
 	private static void P1Test() {
 		System.out.println("======P1 TEST BEGIN======");
-		String[] input = new String[4];
-		input[0] = "3";
-		input[1] = "A 10 30";
-		input[2] = "B 30 5";
-		input[3] = "C 5 60";
+		String[] input = new String[6];
+		input[0] = "5";
+		input[1] = "A 3 4";
+		input[2] = "B 4 5";
+		input[3] = "C 5 20";
+		input[4] = "D 20 60";
+		input[5] = "E 60 25";
+		
+		for(int i = 0; i < input.length; i++) {
+			System.out.println(input[i]);
+		}
 		
 		P5Code.assignTable(input);
 		System.out.println();
 		
 		input = new String[6];
 		input[0] = "5";
-		input[1] = "A 4 10";
-		input[2] = "B 10 3";
-		input[3] = "C 3 12";
-		input[4] = "D 12 20";
-		input[5] = "E 20 7";
+		input[1] = "A 100 500";
+		input[2] = "B 500 325";
+		input[3] = "C 325 700";
+		input[4] = "D 700 100";
+		input[5] = "E 100 500";
+		
+		for(int i = 0; i < input.length; i++) {
+			System.out.println(input[i]);
+		}
 		
 		P5Code.assignTable(input);
 		System.out.println();
 		
-		input = new String[5];
-		input[0] = "4";
-		input[1] = "A 30 35";
+		input = new String[8];
+		input[0] = "7";
+		input[1] = "A 15 35";
 		input[2] = "B 35 15";
-		input[3] = "C 15 5";
-		input[4] = "D 5 10";
+		input[3] = "C 15 15";
+		input[4] = "D 15 20";
+		input[5] = "E 20 5";
+		input[6] = "F 5 35";
+		input[7] = "G 35 5";
+		
+		for(int i = 0; i < input.length; i++) {
+			System.out.println(input[i]);
+		}
 		
 		P5Code.assignTable(input);
 		System.out.println();
@@ -53,45 +70,61 @@ public class P5_TestRunner {
 		input[2] = "15 8";
 		input[3] = "16 9";
 		input[4] = "16 6";
-		input[5] = "10 2";
+		input[5] = "11 2";
 		input[6] = "21 9";
-		input[7] = "18 4";
-		input[8] = "12 4";
-		input[9] = "17 8";
+		input[7] = "18 8";
+		input[8] = "12 14";
+		input[9] = "17 38";
 		input[10] = "18 9";
 		
+		for(int i = 0; i < input.length; i++) {
+			System.out.println(input[i]);
+		}
+		System.out.println();
 		P5Code.partyBudget(input);
 		System.out.println();
 		
-		input = new String[11];
-		input[0] = "50 10";
-		input[1] = "13 8";
-		input[2] = "19 10";
-		input[3] = "16 8";
-		input[4] = "12 9";
-		input[5] = "10 2";
+		input = new String[15];
+		input[0] = "220 14";
+		input[1] = "1 15";
+		input[2] = "200 10";
+		input[3] = "160 16";
+		input[4] = "82 9";
+		input[5] = "1 2";
 		input[6] = "12 8";
-		input[7] = "13 5";
-		input[8] = "15 5";
+		input[7] = "14 3";
+		input[8] = "15 3";
 		input[9] = "11 7";
-		input[10] = "16 2";
+		input[10] = "2 2";
+		input[11] = "5 5";
+		input[12] = "6 5";
+		input[13] = "7 7";
+		input[14] = "8 2";
 		
+		for(int i = 0; i < input.length; i++) {
+			System.out.println(input[i]);
+		}
+		System.out.println();
 		P5Code.partyBudget(input);
 		System.out.println();
 		
 		input = new String[11];
-		input[0] = "50 10";
-		input[1] = "13 4";
-		input[2] = "25 10";
-		input[3] = "5 5";
-		input[4] = "5 5";
-		input[5] = "5 6";
-		input[6] = "5 10";
-		input[7] = "15 1";
-		input[8] = "15 10";
-		input[9] = "7 7";
-		input[10] = "20 10";
+		input[0] = "100 10";
+		input[1] = "99 99";
+		input[2] = "98 98";
+		input[3] = "97 97";
+		input[4] = "96 96";
+		input[5] = "95 95";
+		input[6] = "94 94";
+		input[7] = "93 93";
+		input[8] = "92 92";
+		input[9] = "91 91";
+		input[10] = "90 90";
 		
+		for(int i = 0; i < input.length; i++) {
+			System.out.println(input[i]);
+		}
+		System.out.println();
 		P5Code.partyBudget(input);
 		System.out.println();
 		
@@ -100,21 +133,21 @@ public class P5_TestRunner {
 	private static void P3Test() {
 		
 		System.out.println("======P3 TEST BEGIN======");
-		int l = 100;
-		int cuts = 3;
-		int[] places = new int[]{25, 50, 75};
+		int l = 500;
+		int cuts = 10;
+		int[] places = new int[]{25, 50, 75, 80, 85, 200, 250, 400, 495, 499};
 		
 		P5Code.cut(l, cuts, places);
 		
-		l = 10;
-		cuts = 4;
-		places = new int[]{4, 5, 7, 8};
+		l = 50;
+		cuts = 10;
+		places = new int[]{1, 2, 3, 4, 5, 40, 45, 46, 47, 50};
 		
 		P5Code.cut(l, cuts, places);
 		
-		l = 40;
+		l = 1000;
 		cuts = 5;
-		places = new int[]{5, 10, 15, 20, 25};
+		places = new int[]{100, 200, 555, 750, 957};
 		
 		P5Code.cut(l, cuts, places);
 	}
